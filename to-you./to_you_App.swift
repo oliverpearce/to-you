81b5d97af37d2c9224d0c50f8312424838be42c1
Oliver Pearce <OliverPearce13@gmail.com>
2025-10-02 15:57:38 -0700

    Initial Commit

---
+++ to-you./to_you_App.swift
@@ -0,0 +1,17 @@
+//
+//  to_you_App.swift
+//  to-you.
+//
+//  Created by Silver on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct to_you_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ to-you.Tests/to_you_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  to_you_Tests.swift
+//  to-you.Tests
+//
+//  Created by Silver on 10/2/25.
+//
+
+import Testing
+@testable import to_you_
+
+struct to_you_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
